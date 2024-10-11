bb4f71e3313a255144263ce9b7008a35e490da4c
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2024-10-11 11:22:24 +0800

    finish collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,11 +1,25 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Angel Jimenez
  */
 public class Collatz {
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
 
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
     }
+
+    /** Yields the next number in the Collatz sequence given n. */
+    public static int nextNumber(int currentNumber) {
+        if (currentNumber % 2 == 0)  {
+            return currentNumber / 2;
+        } else {
+            return 3 * currentNumber + 1;
+        }
+    }
+
 }
 
